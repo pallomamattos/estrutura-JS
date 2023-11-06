02a477c17b9a65ce5c472d660e9eab6fc2ce789e
Paloma dos Santos Matos <paloma_matos@bancodigimais.com.br>
2023-11-06 11:11:13 -0300

    repositorio estrutura_js atualizado

---
+++ Estrutura_JS.js
@@ -0,0 +1,59 @@
+// Variáveis:
+// var, let, e const são usados para declarar variáveis.
+// Exemplo: var x = 10;
+
+// Tipos de Dados:
+// Tipos primitivos incluem números, strings, booleanos, nulos e indefinidos.
+// Exemplo: var nome = "João";
+
+// Estruturas de Controle:
+// if, else if, e else para controle condicional.
+// for, while, e do...while para loops.
+
+//Exemplo:
+if (idade >= 18) {
+    console.log("Pode votar!");
+  } else {
+    console.log("Não pode votar.");
+  }
+
+ // Objetos:
+ // Os objetos são coleções de pares chave-valor.
+
+ // Exemplo:
+ar pessoa = {
+    nome: "Paloma",
+    idade: 31
+  };
+
+ // Arrays:
+ // Arrays são coleções ordenadas de valores.
+
+ // Exemplo:
+var frutas = ["maçã", "banana", "laranja"];
+
+ // Eventos:
+ // Você pode lidar com eventos do DOM usando JavaScript.
+ 
+ // Exemplo:
+ document.getElementById("meuBotao").addEventListener("click", function() {
+    alert("Botão clicado!");
+  });
+
+ // Manipulação do DOM:
+ //Você pode interagir com elementos da página web usando o DOM (Document Object Model).
+
+ // Exemplo:
+ var elemento = document.getElementById("minhaDiv");
+elemento.innerHTML = "Novo conteúdo!";
+
+// Ajax:
+// Você pode fazer requisições assíncronas para servidores usando XMLHttpRequest ou fetch.
+
+// Manipulação de Strings e Números:
+//JavaScript oferece muitos métodos para manipular strings e números.
+
+// Expressões Regulares:
+// As expressões regulares permitem fazer correspondências de padrões em strings.
+// Exceções:
+// Você pode usar try, catch, e throw para lidar com erros.
